a5a726bcb58cf1d56901133f897183b1f81782ae
Horatiu Halmaghi <hhalmaghi@gmail.com>
2019-12-02 18:06:20 -0500

    adds subgoal labels to code example

--- lesson-planning/code-examples/client.js
+++ lesson-planning/code-examples/client.js
@@ -1,24 +1,35 @@
+// Require the net module for working with TCP, and the stdin module for
+// receiving input form the command line
 const net = require('net');
 const stdin = process.stdin;
 
 // Interpret incoming data from stdin as text
 stdin.setEncoding('utf8');
 
+// Connect to the server on localhost, port 4000
 const client = net.createConnection({
     host: 'localhost',
     port: 4000
 });
 
+// Expect text input over the TCP connection
+client.setEncoding('utf8');
+
+// Listen for the connect event, which triggers when the connection to the
+// server has been established
 client.on('connect', function() {
     console.log('Connected to the server.');
 });
 
+// Listen for data incoming from the server
 client.on('data', function(data) {
     console.log(data);
 });
 
-client.setEncoding('utf8');
-
+// Listen for data incoming from the command line
 stdin.on('data', function(data) {
+
+    // Send user input to the server
     client.write(data);
 });
+
--- lesson-planning/code-examples/server.js
+++ lesson-planning/code-examples/server.js
@@ -1,28 +1,45 @@
+// Require the net module for working with TCP
 const net = require('net');
 
+// Create TCP server
 const server = net.createServer();
+
+// Declare an array for keeping track of all connected clients
+// and a function for sending messages to all users
 const allClients = [];
 
 const broadcast = function(message) {
     console.log('Broadcasting to all clients: ' + message);
+
+    // Send a message to every connected client
     for (const connectedClient of allClients) {
         connectedClient.write(message);
     }
 };
 
+// Listen for connection events
 server.on('connection', function(connectedClient){
+    // Expect text input from the newly connected client
     connectedClient.setEncoding('utf8');
+
+    // Message all connected clients about the new connection
     broadcast('A new client has connected.');
 
+    // Add the newly connected client to the list of all the others
     allClients.push(connectedClient);
 
+    // Listen for incoming data for the newly connected client
     connectedClient.on('data', function(data) {
+        // Broadcast data from client to every client
         broadcast(data);
     });
 
+    // Send a welcome message to the newly connected client
     connectedClient.write('Welcome!');
+
     console.log('New connection established.');
 });
 
+// Have the serve listen on port 4000
 server.listen(4000);
 
